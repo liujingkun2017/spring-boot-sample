d04a2a489b7049339d6617bfabad0e9f926347eb
joy <13164432553@163.com>
2018-07-28 17:19:34 +0800

    自定义配置文件调试

---
+++ src/main/java/org/liujk/spring/boot/sample/config/PersonCustomConfig.java
@@ -0,0 +1,42 @@
+package org.liujk.spring.boot.sample.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ConfigurationProperties(prefix = "custom")
+@PropertySource(value = "classpath:custom.properties")
+public class PersonCustomConfig {
+
+    private String name;
+
+    private Integer age;
+
+    private String phone;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
--- src/main/java/org/liujk/spring/boot/sample/controller/HelloController.java
+++ src/main/java/org/liujk/spring/boot/sample/controller/HelloController.java
@@ -1,15 +1,51 @@
 package org.liujk.spring.boot.sample.controller;
 
 
+import org.liujk.spring.boot.sample.config.PersonConfig;
+import org.liujk.spring.boot.sample.config.PersonCustomConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableConfigurationProperties({PersonConfig.class})
 public class HelloController {
+
+
+    @Value("${my.name}")
+    private String name;
+
+    @Value("${my.age}")
+    private Integer age;
+
+    @Value("${my.phone}")
+    private String phone;
+
+    @Autowired
+    private PersonConfig personConfig;
+
+    @Autowired
+    private PersonCustomConfig personCustomConfig;
 
     @RequestMapping("/hello-world")
     public String index() {
-        return "Greetings from hello world!";
+
+        String res1 = "name:" + name
+                + " age:" + age
+                + " phone:" + phone;
+
+        String res2 = "tom.name:" + personConfig.getName()
+                + " tom.age:" + personConfig.getAge()
+                + " tom.phone:" + personConfig.getPhone();
+
+        String res3 = "custom.name:" + personCustomConfig.getName()
+                + " custom.age:" + personCustomConfig.getAge()
+                + " custom.phone:" + personCustomConfig.getPhone();
+
+
+        return res1 + res2 + res3 + " Greetings from hello world!";
     }
 
 }
